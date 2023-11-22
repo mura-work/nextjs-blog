44b7aa73226d17fef27ccee808fafa17b47f8a56
mura-work <murata.naoya.work@gmail.com>
2023-11-22 22:45:42 +0900

    [feat] フォームの表示まで

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,26 @@
 import { IconButton } from "@chakra-ui/react";
-import { HamburgerIcon, SearchIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, SearchIcon, EditIcon } from "@chakra-ui/icons";
 
-export const HeaderComponent = () => {
+type PropsType = {
+  openNewTodo: () => void;
+};
+
+export const HeaderComponent = (props: PropsType) => {
   return (
     <header className="flex justify-between bg-cyan-200 px-4">
       <div className="flex items-center">
         <span>TodoApp</span>
       </div>
       <div className="flex">
+        <div className="mr-4">
+          <IconButton
+            variant="unstyled"
+            className="!min-w-0 !min-h-0"
+            aria-label="Search database"
+            icon={<EditIcon />}
+            onClick={() => props.openNewTodo()}
+          />
+        </div>
         <div className="mr-4">
           <IconButton
             variant="unstyled"
